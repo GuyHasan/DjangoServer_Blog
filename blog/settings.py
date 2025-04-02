3543783d520c19493d0a8b4f8e44ae5be26c90c3
GuyHasan <guy28901@gmail.com>
2025-04-02 19:23:15 +0300

    Switch database backend to PostgreSQL and update configuration settings

--- blog/settings.py
+++ blog/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='blog_db'),
+        'USER': config('DB_USER', default='blog_user'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
